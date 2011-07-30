42615ebe60ae8104de102fde39e348dc0dfbea51
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2011-07-30 21:49:06 +0700

    - Support AppConf tag for loading application specific configuration - Support image resizing in CreateMemImage

--- include/AREngineWX/CreateMemImage.h
+++ include/AREngineWX/CreateMemImage.h
@@ -5,6 +5,7 @@
 #include "wx/image.h"
 #include "arenginewx/Export"
 
+#include <osg/Image>
 #include <osgViewer/ViewerEventHandlers>
 using namespace osgViewer;
 
@@ -19,11 +20,12 @@ namespace arenginewx
 	class ARENGINEWX_EXPORT CreateMemImage : public osgViewer::ScreenCaptureHandler::CaptureOperation
 	{
 	public:
-		CreateMemImage(wxWindow *win, ProcessCaptureImageCallback *callback);
+		CreateMemImage(wxWindow *win, ProcessCaptureImageCallback *callback, int width=-1, int height=-1);
 		virtual void operator()(const osg::Image& image, const unsigned int context_id);
 
 	private:
 		wxWindow *m_win;
+		int m_width, m_height;
 		ProcessCaptureImageCallback *m_callback;
 	};
 
--- src/AREngineWX/CreateMemImage.cpp
+++ src/AREngineWX/CreateMemImage.cpp
@@ -1,26 +1,43 @@
 #include "arenginewx/CreateMemImage.h"
 #include "arengine/Util.h"
 #include <osgDB/WriteFile>
+
+using namespace osg;
 using namespace arenginewx;
 
-CreateMemImage::CreateMemImage(wxWindow *win, ProcessCaptureImageCallback *callback)
+CreateMemImage::CreateMemImage(wxWindow *win, ProcessCaptureImageCallback *callback,
+							   int width, int height)
 :m_win(win),
+m_width(width),
+m_height(height),
 m_callback(callback)
 {
+	wxInitAllImageHandlers();
 }
 
 
 void 
 CreateMemImage::operator()(const osg::Image& image, const unsigned int context_id)
 {
-	int w, h;
 	if (m_win != NULL && m_callback != NULL)
 	{
 		int time = arengine::Util::getElapseTimeInMilliSec();
 		stringstream sstr;
 		sstr << "images/" <<  time << ".jpg";
 		wxString fileName = wxT("images/") + wxString::Format(wxT("%i"), time) + wxT(".jpg");
-		osgDB::writeImageFile(image, sstr.str().c_str());
+
+		// resize image if needed
+		if (m_width > 0 && m_height > 0)
+		{
+			ref_ptr<osg::Image> resizeImage = (osg::Image*) image.clone(osg::CopyOp::SHALLOW_COPY);
+			resizeImage->scaleImage(m_width, m_height, 1);
+			osgDB::writeImageFile(*resizeImage.get(), sstr.str().c_str());
+		}
+		else
+		{
+			osgDB::writeImageFile(image, sstr.str().c_str());
+		}
+
 		wxImage wxImg;
 		wxImg.LoadFile(fileName, wxBITMAP_TYPE_JPEG);
 		
